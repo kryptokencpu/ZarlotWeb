8dd7d17a2ffa9d5013c04fc4a7e1909b68b0f43f
Ken <116120394+kryptokencpu@users.noreply.github.com>
2023-12-26 18:43:07 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import logo from './logo.svg';
+import React  from 'react';
+import myGif from "./myGif.gif"
 import './App.css';
+import CountdownTimer from './CountdownTimer';
 
 function App() {
+
+  const targetDate = "2023-12-31T23:59:59";
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Zarlox Invaders</h1>
+        <img src={myGif} alt="Zarlot Invader" />
+        <CountdownTimer targetDate={targetDate} />
+        {/* Add your countdown component here */}
+        <div className="buttons">
+          <a href="https://twitter.com/ZarloxInvaders" target="_blank" rel="noopener noreferrer" className="btn">Twitter</a>
+          <a href="https://discord.com/yourdiscordlink" target="_blank" rel="noopener noreferrer" className="btn">Discord [COMING SOON]</a>
+        </div>
       </header>
     </div>
   );
