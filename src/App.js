c500915ad9ea90aeb643fcfda50a83011ca672d3
Ken <116120394+kryptokencpu@users.noreply.github.com>
2024-01-08 17:55:50 -0500

    rebrand

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import React  from 'react';
 import myGif from "./myGif.gif"
 import './App.css';
-import CountdownTimer from './CountdownTimer';
+
 
 function App() {
 
-  const targetDate = "2023-12-29T23:59:59";
+
 
   return (
     <div className="App">
       <header className="App-header">
         <h1>Zarlox Invaders</h1>
-        <h2>2,000 Eridian NFTs</h2>
+        <h2>500 NFTs</h2>
         <h3>0.25 SOL Mint</h3>
-        <img src={myGif} alt="Zarlot Invader" />
-        <CountdownTimer targetDate={targetDate} />
+        <img src={myGif} alt="Zarlot Invader" className="myImage"/>
+        
         {/* Add your countdown component here */}
         <div className="buttons">
           <a href="https://twitter.com/ZarloxInvaders" target="_blank" rel="noopener noreferrer" className="btn">Twitter</a>
